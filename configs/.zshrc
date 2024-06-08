7bf6acc422d16f263fd0a263aed121cdc65af302
Rodrigo Picanço <rodrigo.picanto@rodrigo.picanco>
2024-06-08 12:36:08 +0200

    Update with minimal configs from coder branch

--- configs/.zshrc
+++ configs/.zshrc
@@ -1,8 +1,5 @@
-plug "jeffreytse/zsh-vi-mode"
-
 eval "$(zoxide init zsh)"
 eval "$(starship init zsh)"
-eval "$(tmuxifier init -)"
 
 export EDITOR=nvim
 export VISUAL=nvim
--- install.sh
+++ install.sh
@@ -1,46 +1,18 @@
 #! /bin/bash
-lazygit() {
-        LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-        curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-        tar xf lazygit.tar.gz lazygit
-        sudo install lazygit /usr/local/bin
-}
-tmux() {
-        DEBIAN_FRONTEND=noninteractive sudo apt-get install tmux -yq
-}
-zoxide() {
-        DEBIAN_FRONTEND=noninteractive sudo apt-get install zoxide -y
-}
-stow() {
-        DEBIAN_FRONTEND=noninteractive sudo apt-get install stow -y
-}
-
 packages() {
-        lazygit
-        stow
-        tmux 
-        zoxide 
+        brew install lazygit stow tmux zoxide neovim starship
 }
 
 tpm() {
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
-zap() {
-        zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
-}
 
 package_managers(){
         tpm
-        zap
 }
 
 symlinks() {
-        DOTFILES_PATH=$(pwd)
-        pushd ~
-                cp $DOTFILES_PATH -R dotfiles
-                rm .zshrc
-        popd
-        cd ~/dotfiles
+	mv ~/.zshrc ~/.zshrc.bak
         stow configs
 }
 
